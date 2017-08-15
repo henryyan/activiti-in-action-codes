90b8d4c6ef8cd3a284f823abeffc079e34d0a422
mashiguang <mashiguang0833@sina.com>
2017-08-15 16:58:40 +0800

    identityService test

---
+++ chapter5-oa-manager/src/test/java/cn/niceabc/chapter5/IdentityServiceTest.java
@@ -0,0 +1,126 @@
+package cn.niceabc.chapter5;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class IdentityServiceTest {
+
+    @Rule
+    // 默认加载activiti.cfg.xml
+    public ActivitiRule activitiRule = new ActivitiRule();
+
+    @Test
+    public void testUser() {
+        IdentityService identityService = activitiRule.getIdentityService();
+
+        identityService.deleteUser("tom");
+
+        User user = identityService.newUser("tom");
+        user.setFirstName("tom");
+        user.setLastName("cruise");
+        user.setEmail("tom.cruise@email.com");
+        identityService.saveUser(user);
+
+        User tom = identityService.createUserQuery().userId("tom").singleResult();
+        Assert.assertNotNull(tom);
+        Assert.assertEquals("cruise", tom.getLastName());
+
+
+        identityService.deleteUser("tom");
+
+        User tom_null = identityService.createUserQuery().userId("tom").singleResult();
+        Assert.assertNull(tom_null);
+
+    }
+
+    @Test
+    public void testGroup() {
+        IdentityService identityService = activitiRule.getIdentityService();
+
+        identityService.deleteGroup("development");
+
+        Group group  =identityService.newGroup("development");
+        group.setName("development");
+        group.setType("assignment");
+        identityService.saveGroup(group);
+
+        Group group1 = identityService.createGroupQuery().groupId("development").singleResult();
+        Assert.assertNotNull(group1);
+        Assert.assertEquals("development", group1.getName());
+        Assert.assertEquals("assignment", group1.getType());
+
+        identityService.deleteGroup("development");
+
+        Group group_null = identityService.createGroupQuery().groupId("development").singleResult();
+        Assert.assertNull(group_null);
+
+    }
+
+    @Test
+    public void testMembership() {
+        IdentityService identityService = activitiRule.getIdentityService();
+
+        identityService.deleteMembership("tom", "development");
+        identityService.deleteGroup("development");
+        identityService.deleteUser("tom");
+
+        //group
+
+        Group group  =identityService.newGroup("development");
+        group.setName("development");
+        group.setType("assignment");
+        identityService.saveGroup(group);
+
+        Group group1 = identityService.createGroupQuery().groupId("development").singleResult();
+        Assert.assertNotNull(group1);
+        Assert.assertEquals("development", group1.getName());
+        Assert.assertEquals("assignment", group1.getType());
+
+        //user
+
+        User user = identityService.newUser("tom");
+        user.setFirstName("tom");
+        user.setLastName("cruise");
+        user.setEmail("tom.cruise@email.com");
+        identityService.saveUser(user);
+
+        User tom = identityService.createUserQuery().userId("tom").singleResult();
+        Assert.assertNotNull(tom);
+        Assert.assertEquals("cruise", tom.getLastName());
+
+
+        // membership
+        identityService.createMembership("tom", "development");
+        User tomInGroup = identityService
+                .createUserQuery()
+                .memberOfGroup("development")
+                .singleResult();
+        Assert.assertNotNull(tomInGroup);
+        Assert.assertEquals("tom", tomInGroup.getId());
+
+        Group groupContainsTom = identityService
+              .createGroupQuery()
+                .groupMember("tom")
+                .singleResult();
+        Assert.assertNotNull(groupContainsTom);
+        Assert.assertEquals("development", groupContainsTom.getId());
+
+        // clean
+
+        identityService.deleteMembership("tom", "development");
+
+        identityService.deleteUser("tom");
+        User tom_null = identityService.createUserQuery().userId("tom").singleResult();
+        Assert.assertNull(tom_null);
+
+        identityService.deleteGroup("development");
+        Group group_null = identityService.createGroupQuery().groupId("development").singleResult();
+        Assert.assertNull(group_null);
+
+    }
+}
---
+++ chapter5-oa-manager/src/test/java/cn/niceabc/junit/StopwatchTest.java
@@ -0,0 +1,60 @@
+package cn.niceabc.junit;
+
+import org.junit.AssumptionViolatedException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+public class StopwatchTest {
+    private static final Logger logger = Logger.getLogger("");
+
+    private static void logInfo(Description description, String status, long nanos) {
+        String testName = description.getMethodName();
+        logger.info(String.format("Test %s %s, spent %d microseconds",
+                testName, status, TimeUnit.NANOSECONDS.toMicros(nanos)));
+    }
+
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void succeeded(long nanos, Description description) {
+            logInfo(description, "succeeded", nanos);
+        }
+
+        @Override
+        protected void failed(long nanos, Throwable e, Description description) {
+            logInfo(description, "failed", nanos);
+        }
+
+        @Override
+        protected void skipped(long nanos, AssumptionViolatedException e, Description description) {
+            logInfo(description, "skipped", nanos);
+        }
+
+        @Override
+        protected void finished(long nanos, Description description) {
+            logInfo(description, "finished", nanos);
+        }
+    };
+
+    @Test
+    public void succeeds() {
+    }
+
+    @Test
+    public void fails() {
+        fail();
+    }
+
+    @Test
+    public void skips() {
+        assumeTrue(false);
+    }
+}
